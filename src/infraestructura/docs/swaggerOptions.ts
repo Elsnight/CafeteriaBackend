6f48e40884f5351337d11a3e419a3d2eb16033bd
Miguel Cuenca <ecuenca4@gmail.com>
2023-01-20 00:12:40 -0500

    make it better

--- src/infraestructura/docs/swaggerOptions.ts
+++ src/infraestructura/docs/swaggerOptions.ts
@@ -1,16 +1,15 @@
-import { PORT } from '../config'
 
 export const swaggerOptions = {
-  definition:{
-    openapi:'3.0.0',
-    info:{
-      title:'API REST Comedor EPN',
-      version:'1.0.0',
-      description:'API REST que sirve los enpoint necesarios para el funcionamiento del comedor EPN'
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API REST Comedor EPN',
+      version: '1.0.0',
+      description: 'API REST que sirve los enpoint necesarios para el funcionamiento del comedor EPN'
     },
-    servers:[
+    servers: [
       {
-        url: `http://localhost:${PORT}`
+        url: `https://app-cafeteriacackend.herokuapp.com/`
       }
     ]
   },
